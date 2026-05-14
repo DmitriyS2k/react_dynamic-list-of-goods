aa93ca4d4aca8c966baa24d0edcdf72daa84a615
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-05-14 15:59:25 +0300

    Implement dynamic goods list loadingÑ

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [preparedGoodsList, setPreparedGoodsList] = React.useState<Good[]>([]);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+  const getDataToRender = (source: Promise<Good[]>): void => {
+    source.then(setPreparedGoodsList);
+  };
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => getDataToRender(getAll())}
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          getDataToRender(get5First());
+        }}
+      >
+        Load 5 first goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          getDataToRender(getRedGoods());
+        }}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={preparedGoodsList} />
+    </div>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Good } from './types/Good';
+import classNames from 'classnames';
 
 type Props = {
   goods: Good[];
 };
 
+const textColorHandler = (value: string) => {
+  switch (value) {
+    case 'red':
+      return 'red-text';
+    case 'green':
+      return 'green-text';
+    case 'blue':
+      return 'blue-text';
+    default:
+      return '';
+  }
+};
+
 export const GoodsList: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        className={classNames(textColorHandler(good.color))}
+      >
         {good.name}
       </li>
     ))}
